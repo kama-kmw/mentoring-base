4e7a58808ac613d51ed45a77b0c616ebc1077a79
kama-kmw <kamil.l2020@yandex.ru>
2025-04-23 14:19:58 +0300

    refactor: task five

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,48 @@
+import { NgFor, NgIf } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+
+const showItemMenu = (item: string) => {
+  return item;
+};
+const itemMenu = showItemMenu('О компании');
+
+const newPages = [5, 4, 3, 2, 1];
+
+const menuElements = [
+  'Каталог ',
+  'Стройматериалы ',
+  'Инструменты ',
+  'Электрика ',
+  'Интерьер и одежда',
+];
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgIf, NgFor],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'mentoring-first-project';
+  
+  isShowBanner = true;
+
   readonly headerNavLink1 = 'Главная';
   readonly headerNavLink2 = 'О компании';
   readonly headerNavLink3 = 'Каталог';
-  readonly headerSubNavLink1 = 'Каталог';
-  readonly headerSubNavLink2 = 'Стройматериалы';
-  readonly headerSubNavLink3 = 'Инструменты';
-  readonly headerSubNavLink4 = 'Электрика';
-  readonly headerSubNavLink5 = 'Интерьер и отделка';
+
+  aboutCompany = itemMenu;
+  newPages = newPages;
+  menuItems = menuElements;
+
+  isUpperCase = true;
+
+  changeMenuText() {
+    this.menuItems = menuElements.map((item) =>
+      this.isUpperCase ? item.toLowerCase() : item.toUpperCase()
+    );
+    this.isUpperCase = !this.isUpperCase;
+  }
 }
