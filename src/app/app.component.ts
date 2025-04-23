f2d7a2442e57b2213f355054769d01c5f52587f6
kama-kmw <kamil.l2020@yandex.ru>
2025-04-23 07:02:26 +0300

    refactor: section header, task-five

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,8 +6,16 @@ import { RouterOutlet } from '@angular/router';
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'mentoring-first-project';
+  readonly headerNavLink1 = 'Главная';
+  readonly headerNavLink2 = 'О компании';
+  readonly headerNavLink3 = 'Каталог';
+  readonly headerSubNavLink1 = 'Каталог';
+  readonly headerSubNavLink2 = 'Стройматериалы';
+  readonly headerSubNavLink3 = 'Инструменты';
+  readonly headerSubNavLink4 = 'Электрика';
+  readonly headerSubNavLink5 = 'Интерьер и отделка';
 }
