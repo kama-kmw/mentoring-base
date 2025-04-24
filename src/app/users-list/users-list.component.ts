2bc79b0e0d861725aae843e3c0ccad832ce7fc4e
kama-kmw <kamil.l2020@yandex.ru>
2025-04-24 17:30:06 +0300

    feat: add component UsersListComponent

---
+++ src/app/users-list/users-list.component.ts
@@ -0,0 +1,37 @@
+import { NgFor, NgIf } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, Injectable } from '@angular/core';
+
+const apiService = 'dsdsdsd';
+
+@Component({
+  selector: 'app-users-list',
+  templateUrl: './users-list-component.html',
+  styleUrl: './usets-list-component.scss',
+  standalone: true,
+  imports: [NgFor],
+})
+export class UsersListComponent {
+  readonly apiService = inject(HttpClient);
+  users: any = [];
+
+  constructor() {
+    this.apiService
+      .get('https://jsonplaceholder.typicode.com/users')
+      .subscribe((response: any) => {
+        this.users = response;
+        console.log('Users:', this.users);
+      });
+  }
+
+  deleteUser(id: number) {
+    // this.users = this.users.filter((item: any) => item.id !== id);
+    this.users = this.users.filter((item: any) => {
+      if (id === item.id) {
+        return false;
+      } else {
+        return true;
+      }
+    });
+  }
+}
